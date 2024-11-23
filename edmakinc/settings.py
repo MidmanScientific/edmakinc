20543ca15ed3f720711350a3ed9bfe25cfe4e51d
Emmanuel Makae <marnuel19clerk@gmail.com>
2024-11-23 09:38:29 -0800

    first commit

--- edmakinc/settings.py
+++ edmakinc/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 # Application definition
 
 INSTALLED_APPS = [
+    'django_daisy',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
