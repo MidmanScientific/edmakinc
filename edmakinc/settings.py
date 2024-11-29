691da0b67985f6ac1d00922cd97ab22123cb80ff
Emmanuel Makae <marnuel19clerk@gmail.com>
2024-11-29 01:48:11 -0800

    first commit

--- edmakinc/settings.py
+++ edmakinc/settings.py
@@ -135,6 +135,23 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+if DEBUG:
+
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+
 import os
 
 MEDIA_URL = '/media/'  # URL to serve media files
