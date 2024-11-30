0113064ec1516d68b694cca41ad3e41e4f379a9a
Emmanuel Makae <marnuel19clerk@gmail.com>
2024-11-30 07:45:58 -0800

    first commit

--- edmakFeatures/urls.py
+++ edmakFeatures/urls.py
@@ -5,8 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -33,11 +31,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
 
 ]
- 
 
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
